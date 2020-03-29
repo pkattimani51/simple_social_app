229d7db8139cbc622b265d444103007364afc7ca
Prashant Kattimani -X (prkattim - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prkattim@partnet.cisco.com>
2020-03-29 21:43:48 +0530

    added whitenoise middleware line

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'simplesocial.urls'
@@ -123,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 # STATICFILES_DIRS = [STATIC_ROOT]
 
 LOGIN_REDIRECT_URL = 'test'
